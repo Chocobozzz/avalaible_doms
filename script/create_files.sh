17882f0a9509b696f027552cf3a4c0b67035450e
Chocobozzz <djidane14ff@hotmail.fr>
2014-01-20 14:57:36 +0100

    Add files of domain names

--- script/create_files.sh
+++ script/create_files.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/sh
 
-rm ndm2.txt;
-rm ndm3.txt;
+for extension in .com .fr .net .org; do
+    rm ndm2$extension;
+    rm ndm3$extension;
 
-for i in 0 1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n o p q r s t u v w x y z; do
-    for j in 0 1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n o p q r s t u v w x y z; do 
-        echo $i$j.com >> ndm2.txt; 
-    done; 
+    for i in 0 1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n o p q r s t u v w x y z; do
+        for j in 0 1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n o p q r s t u v w x y z; do 
+            echo $i$j$extension >> ndm2$extension; 
+        done; 
+    done;
+
+    for i in 0 1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n o p q r s t u v w x y z; do
+        for j in 0 1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n o p q r s t u v w x y z; do 
+            for k in 0 1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n o p q r s t u v w x y z; do 
+                echo $i$j$k$extension >> ndm3$extension; 
+            done;
+        done; 
+    done;
 done;
 
-for i in 0 1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n o p q r s t u v w x y z; do
-    for j in 0 1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n o p q r s t u v w x y z; do 
-        for k in 0 1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n o p q r s t u v w x y z; do 
-            echo $i$j$k.com >> ndm3.txt; 
-        done;
-    done; 
-done;
 
 
